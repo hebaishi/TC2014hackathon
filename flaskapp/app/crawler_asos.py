6e4ad5ab49943511fe3518ad0c250facc9f23748
zoulzubazz <zoulzubazz@googlemail.com>
2014-10-18 19:50:28 +0100

    asos crawl script

---
+++ flaskapp/app/crawler_asos.py
@@ -0,0 +1,25 @@
+import requests
+import bs4
+import re
+
+root_url = 'http://pyvideo.org'
+index_url = root_url + '/category/50/pycon-us-2014'
+
+url = 'http://www.asos.com/search/shirt?q=shirt'
+
+def get_video_page_urls():
+    response = requests.get(index_url)
+    soup = bs4.BeautifulSoup(response.text)
+
+if __name__ == '__main__':
+    response = requests.get(url)
+    soup = bs4.BeautifulSoup(response.text)
+    prodlink = list()    
+    for link in soup.find_all('a'):
+         if (re.search(r'iid',str(link.get('href')))):        
+            prodlink.append(str(link.get('href'))) 
+            for plink in range(len(prodlink) - 1):
+                responsep = requests.get(prodlink[0])
+                soupp = bs4.BeautifulSoup(responsep.text)
+                re.findall('images', soupp)
+                
