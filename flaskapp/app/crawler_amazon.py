371ea281be9bacf62fbbeddaea1f49c587784ff9
hebaishi <hebaishi.husam@gmail.com>
2014-10-18 17:24:10 +0100

    Amazon crawler working

--- flaskapp/app/crawler_amazon.py
+++ flaskapp/app/crawler_amazon.py
@@ -5,7 +5,7 @@ import re
 root_url = 'http://pyvideo.org'
 index_url = root_url + '/category/50/pycon-us-2014'
 
-url = 'http://www.amazon.co.uk/s/ref=nb_sb_noss_2?url=search-alias%3Dclothing&field-keywords=skirt'
+url = 'http://www.amazon.co.uk/gp/aw/s/ref=is_box_?k=skirt'
 
 def get_video_page_urls():
     response = requests.get(index_url)
@@ -15,5 +15,4 @@ if __name__ == '__main__':
     response = requests.get(url)
     soup = bs4.BeautifulSoup(response.text)
     for link in soup.find_all('a'):
-        if (re.search(r'keyword',str(link.get('href')))):        
-            print(link.get('href'))
+        print(link.string)
