4d7643f848a0122285a78e3f35468bd7588ff2ea
zoulzubazz <zoulzubazz@googlemail.com>
2014-10-18 19:45:19 +0100

    asos script update

--- flaskapp/app/crawler_ebay.py
+++ flaskapp/app/crawler_ebay.py
@@ -5,7 +5,7 @@ import re
 root_url = 'http://pyvideo.org'
 index_url = root_url + '/category/50/pycon-us-2014'
 
-url = 'http://mobile.ebay.co.uk/Pages/SearchResults.aspx?sflag=1&emvcc=0&nbcol=0|null'
+url = 'http://www.asos.com/search/shirt?q=shirt'
 
 def get_video_page_urls():
     response = requests.get(index_url)
@@ -14,6 +14,9 @@ def get_video_page_urls():
 if __name__ == '__main__':
     response = requests.get(url)
     soup = bs4.BeautifulSoup(response.text)
+    prodlink = list()    
     for link in soup.find_all('a'):
-         #if (re.search(r'aid',str(link.get('href')))):        
-            print(link.get('href'))
+         if (re.search(r'iid',str(link.get('href')))):        
+            prodlink.append(str(link.get('href'))) 
+            
+            
