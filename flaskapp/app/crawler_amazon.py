73d400ed5deef023d88acf02391e8732b2245195
hebaishi <hebaishi.husam@gmail.com>
2014-10-18 18:11:34 +0100

    Amazon crawler updated, pulling price now

--- flaskapp/app/crawler_amazon.py
+++ flaskapp/app/crawler_amazon.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import requests
 import bs4
 import re
@@ -5,14 +6,24 @@ import re
 root_url = 'http://pyvideo.org'
 index_url = root_url + '/category/50/pycon-us-2014'
 
-url = 'http://www.amazon.co.uk/gp/aw/s/ref=is_box_?k=skirt'
+url = 'http://www.amazon.co.uk/gp/aw/s/ref=is_box_?k=trousers'
 
-def get_video_page_urls():
-    response = requests.get(index_url)
-    soup = bs4.BeautifulSoup(response.text)
 
 if __name__ == '__main__':
     response = requests.get(url)
     soup = bs4.BeautifulSoup(response.text)
     for link in soup.find_all('a'):
-        print(link.string)
+        if (re.search('qid=',str(link.get('href')))):        
+            line = ""
+            line = "DESC " + str(link.string)
+            line += " LINK " + str(link.get('href'))        
+            prodlink = "http://www.amazon.co.uk/"+str(link.get('href'))
+            response2 = requests.get(prodlink)
+            soup2 = bs4.BeautifulSoup(response2.text)        
+            for imgs in soup2.find_all('img'):
+                if (str(imgs.get('id')) == "detailImg"):            
+                    line += "IMG " + imgs.get('src')
+            m=re.findall('£(\d+)\.(\d+)',str(response2.text),re.I)
+            price = str(m.group(1)) + "." + str(m.group(2))        
+            line += " PRICE "+ price
+            print line
