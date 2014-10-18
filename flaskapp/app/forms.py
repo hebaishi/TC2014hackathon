d665220bd47f6adc59057a815d5cca6b3d38b06f
hebaishi <hebaishi.husam@gmail.com>
2014-10-18 15:51:07 +0100

    User interface complete

---
+++ flaskapp/app/forms.py
@@ -0,0 +1,10 @@
+from flask.ext.wtf import Form
+from wtforms import TextField, TextAreaField, SubmitField, IntegerField, RadioField
+from wtforms.validators import Required
+
+class Params(Form):
+  height = IntegerField("Height (in cm)")
+  weight = IntegerField("Weight")
+  gender = RadioField('Gender', choices=[('Male','Male'),('Female','female')])
+  keywords = TextField("Keywords")
+  submit = SubmitField("Send")
--- flaskapp/app/routes.py
+++ flaskapp/app/routes.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
- 
+from flask import Flask, render_template,request, flash
+from forms import Params
+
 app = Flask(__name__)     
+app.secret_key = 'development key'
  
-@app.route('/')
-def home():
-  return render_template('home.html')
+@app.route('/',methods = ['GET', 'POST'])
+def params():
+  form = Params()
+ 
+  if request.method == 'POST':
+    if form.validate() == False:
+      flash('All fields are required.')
+      flash(form.height.data)
+      return render_template('params.html', form=form)
+    else:
+      return 'Form posted.'
+ 
+ 
+  elif request.method == 'GET':
+    return render_template('params.html', form=form)
  
 if __name__ == '__main__':
   app.run(debug=True)
